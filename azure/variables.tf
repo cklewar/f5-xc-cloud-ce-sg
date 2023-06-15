113764db1b322554a3645012de9c2e835d32f965
cklewar <cklewar@t-online.de>
2023-06-15 18:41:46 +0200

    0.11.21 v4

--- azure/variables.tf
+++ azure/variables.tf
@@ -10,26 +10,6 @@ variable "f5xc_cluster_name" {
   type = string
 }
 
-variable "azurerm_resource_group_name" {
-  type = string
-}
-
-variable "azurerm_security_group_slo_id" {
-  type = list(object({
-    name                         = string
-    access                       = string
-    priority                     = number
-    protocol                     = string
-    direction                    = string
-    source_port_range            = string
-    source_address_prefix        = optional(string)
-    source_address_prefixes      = optional(list(string))
-    destination_port_range       = string
-    destination_address_prefix   = optional(string)
-    destination_address_prefixes = optional(list(string))
-  }))
-}
-
 variable "f5xc_ip_ranges_Americas_TCP" {
   type = list(string)
 }
